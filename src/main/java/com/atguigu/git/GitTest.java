b7051190666691490612922dcf69b9fb35f384c8
Liuhc1026 <112147361+Liuhc1026@users.noreply.github.com>
2022-11-29 19:56:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lhcstart
+ * @create 2022-11-29 19:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
